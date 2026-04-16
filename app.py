355beaa848782e2d8e46eee9f5641cc6ba15f5f1
JoeyTh04 <104384397@students.swinburne.edu.my>
2026-04-16 11:05:47 +0800

    Update app.py with static image path

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Task 3.1 Complete!</h1>'
+    return '''
+    <h1>Task 3.1 Complete!</h1>
+    <img src="/static/render.jpg" alt="Render Image" style="max-width: 100%;">
+    '''
 
 if __name__ == '__main__':
     app.run()
